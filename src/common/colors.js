6ec033c9e6b5518954abab30a11f0cff8e0bc9f1
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2021-07-08 20:25:40 +0100

    chore: add colors and container component

---
+++ src/common/colors.js
@@ -0,0 +1,13 @@
+export const Purple = "#5956E9";
+export const White = "#FFFFFF";
+export const Black = "#000000";
+export const Gray = "#C9C9C9";
+export const Grey = "#868686";
+export const Ash = "#9A9A9D";
+export const Background = "#F2F2F2";
+export const Border = "#E3E3E3";
+export const SkyBlue = "#7485C1";
+export const RoseGold = "#C9A19C";
+export const Green = "#A1C89B";
+export const DeliveryColour = "#D3F2FF";
+export const PlusBox = "#7DCCEC";
